4d678cbd201fd63a6940bb44aea40b1060e43006
ilief2020 <stanescuflorentina@gmail.com>
2020-05-12 15:09:42 +0300

    Updates

---
+++ precache-manifest.c8d5ae27a036fed9fb992a5d03cca16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4eddf87eb29318096e240ebd43388b",
+    "url": "/stockpriceapp/index.html"
+  },
+  {
+    "revision": "66469511960dcb00f477",
+    "url": "/stockpriceapp/static/css/main.8611a57b.chunk.css"
+  },
+  {
+    "revision": "028862710c01479ff0fd",
+    "url": "/stockpriceapp/static/js/2.696f385a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stockpriceapp/static/js/2.696f385a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66469511960dcb00f477",
+    "url": "/stockpriceapp/static/js/main.603d89d9.chunk.js"
+  },
+  {
+    "revision": "4a76268667f82b8f292e",
+    "url": "/stockpriceapp/static/js/runtime-main.2c41bf5b.js"
+  }
+]);
